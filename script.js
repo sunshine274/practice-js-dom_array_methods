bfd52fd228c5c633cfc302e6952f778f581aeac4
sunshine274 <jingx274@gmail.com>
2020-09-23 21:05:46 +0000

    Finished styling

---
+++ script.js
@@ -0,0 +1,28 @@
+// Select DOM elements
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionairesBtn = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+let data = []; // put all of the people
+
+// Fetch random user and add money
+async function getRandomUser() {
+    const res = await fetch('http://randomuser.me/api');
+    const data = await res.json();
+
+    const user = data.results[0];
+
+    const newUser = {
+        name: `${user.name.first} ${user.name.last}`,
+        money: Math.floor(Math.random()*1000000)
+    };// an object
+    addData(newUser);
+}
+
+// Add new object to data array
+function addData(obj) {
+    data.push(obj);
+}
