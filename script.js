dc67d1765489ad43bb7ee2cc3323bc9ae8cd4d3c
sunshine274 <jingx274@gmail.com>
2020-09-23 21:29:39 +0000

    Added addUserBtn event listener

--- script.js
+++ script.js
@@ -8,9 +8,11 @@ const calculateWealthBtn = document.getElementById('calculate-wealth');
 
 let data = []; // put all of the people
 
+getRandomUser();
+
 // Fetch random user and add money
 async function getRandomUser() {
-    const res = await fetch('http://randomuser.me/api');
+    const res = await fetch('https://randomuser.me/api/');
     const data = await res.json();
 
     const user = data.results[0];
@@ -25,4 +27,27 @@ async function getRandomUser() {
 // Add new object to data array
 function addData(obj) {
     data.push(obj);
+
+    updateDOM();
 }
+
+// Update DOM
+function updateDOM(providedData = data){
+// Clear main div
+main.innerHTML = '<h2><strong>Person</strong>Wealth</h2>';
+
+providedData.forEach(item => {
+const element = document.createElement('div');
+element.classList.add('person');
+element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+main.appendChild(element);
+});
+}
+
+// Format number as money
+function formatMoney(number){
+    return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g,'$&,');
+}
+
+// Event listeners
+addUserBtn.addEventListener('click',getRandomUser);
